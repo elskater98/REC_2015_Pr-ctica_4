afccb001e77d7fb8c4a0eeacadcaf5b310795dc0
Francesc <elskater98@gmail.com>
2019-06-11 19:00:24 +0200

    Machine DONE

--- src/Machine.java
+++ src/Machine.java
@@ -1,3 +1,24 @@
 public class Machine extends MachineComponent{
+
+    private boolean broken = false;
+
+    public void setBroken() {
+        boolean old_broken = this.broken; //save the old value
+        this.broken=true; // update to the new value
+        if(!old_broken){
+            setChanged();
+            notifyObservers();//PULL
+        }
+    }
+
+    public void repair() {
+        boolean old_broken = this.broken; //save the old value
+        this.broken=false; // update to the new value
+        if(old_broken){
+            setChanged();
+            notifyObservers();//PULL
+        }
+    }
+
     public boolean isBroken() { return broken; }
 }

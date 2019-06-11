b8830101484646a744f6d5f8a64ca7d6523f0028
Francesc <elskater98@gmail.com>
2019-06-11 18:41:20 +0200

    Enunciat

---
+++ src/MachineComponent.java
@@ -0,0 +1,8 @@
+public abstract class MachineComponent {
+
+    protected boolean broken = false;
+    public void setBroken() { broken = true; }
+    public void repair() { broken = false; }
+    public abstract boolean isBroken();
+
+}
